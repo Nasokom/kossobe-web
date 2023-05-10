9f46d9e733b28630e8dac0b409c1952023e9dbd9
Mike <124715146+Nasokom@users.noreply.github.com>
2023-05-10 08:37:53 +0200

    fix listItem

--- kossobe-next/component/Ui/ComplexText.jsx
+++ kossobe-next/component/Ui/ComplexText.jsx
@@ -10,27 +10,10 @@ const ComplexText = ({data}) => {
 //todo 
     const Span = ({data, defs}) =>{
   
-      console.log(defs)
+      //console.log(defs)
 
       const customStyle = {} //Style
       const [defIndex, setDefIndex] = useState(null) //Store markDefs index
-       
-
-      /*   data.marks &&  data.marks.forEach((mark)=>{//Select 1 style
-            switch(mark) {
-                case 'strong':
-                    customStyle.fontWeight = 'bolder'
-                  break;
-                case 'underline':
-                    customStyle.textTransform = 'underline'
-                  break;
-                  case 'em':
-                    customStyle.fontStyle = 'italic'
-                  break;
-                default:
-                  // code block
-              }              
-            }) */
       
 
       defs.length >= 1 && defs.forEach((def,i)=>{
@@ -59,24 +42,37 @@ const ComplexText = ({data}) => {
 
           })
 
-        return  customStyle.href ? <Link href={customStyle.href} style={customStyle}>{data.text}</Link>  : <span style={customStyle}>{data.text}</span>
+        return  customStyle.href ? <Link href={customStyle.href} style={customStyle} target='_blank'>{data.text}</Link>  : <span style={customStyle}>{data.text}</span>
        // return <span style={customStyle}>{data.text}</span>
     }
 
     //control mark defs
-    
+    //console.log(data)
+
     return (
       <div className='text-container'>
         {data.map((p,i)=>{
-
           const markDefs = p.markDefs && p.markDefs //markDefs[0]._key && .href &&._type
-            return( 
-            <p key={i}>
+            //const list = p.listItem && p.listItem;
+            
+            if(p.listItem){
+              return( 
+                <ul key={i} style={{listStyle:"inside"}}>
                 {p.children.map((t,i)=>{
-                    return <Span data= {t} key={i} defs={markDefs}/>
+                  return <li style={{listStyle:"inside"}} key={i}><Span data= {t}  defs={markDefs}/> </li>
                 })}
-                
-            </p>)
+            </ul> 
+            )
+          }else{
+            return(
+
+              <p key={i}>
+            {p.children.map((t,i)=>{
+                return <Span data= {t} key={i} defs={markDefs}/>
+              })}
+            </p> 
+            )
+          }
         })}    
     </div>
   )
