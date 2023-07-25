3da64b8b594f861dd08296fbb0999ac358061a52
Mike <124715146+Nasokom@users.noreply.github.com>
2023-07-25 22:04:19 +0200

    d

---
+++ kossobe-next/component/Layout/ComingSoon.jsx
@@ -0,0 +1,11 @@
+
+const ComingSoon = () => {
+    return (
+      <div id="comingSoon">
+         
+              <h1 id="soonText1">Website under <span style={{fontSize:'15vw', backgroundColor:'#FFFFF2', padding:'10px', borderRadius:'20px'}}>🏗️</span></h1>
+              <Example />
+              <h1 id="soonText2">Coming Soon </h1> 
+      </div>
+    )
+  }
